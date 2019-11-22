8bb91824a995832199526c988510b6b6e93756d7
Cristofer Marin <crismaverdeveloper@gmail.com>
2019-11-22 16:10:10 -0600

    feat: Se agrega nuevo flujo de Productos

--- app/src/main/java/com/dot7/firebase/codelabs/activities/MainActivity.kt
+++ app/src/main/java/com/dot7/firebase/codelabs/activities/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setUpRemoteConfig()
+
+        Log.v("xxx","-")
     }
 
     private fun setUpRemoteConfig() {
