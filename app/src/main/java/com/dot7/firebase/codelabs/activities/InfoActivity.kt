de2f1e0fc87d01e4765760869ac490304d906e1c
Cristian Contreras <jes__22222@hotmail.com>
2019-11-22 16:07:49 -0600

    Changes :p

--- app/src/main/java/com/dot7/firebase/codelabs/activities/InfoActivity.kt
+++ app/src/main/java/com/dot7/firebase/codelabs/activities/InfoActivity.kt
@@ -9,5 +9,8 @@ class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        val changes = "Esto es un change :D"
+        Log.d("changes", "$changes")
     }
 }
